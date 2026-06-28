a3463afda0a49ed345c9aaff5cd391d0f737fd21
bojan.vasic <bojan.vasic01@gmail.com>
2026-06-28 21:50:30 +0200

    Add wallet service to dev startup script

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -6,12 +6,14 @@ osascript <<EOF
 tell application "Terminal"
     activate
 
-    do script "cd '$ROOT_DIR' && echo 'Starting Docker infrastructure...' && docker compose up -d && echo '' && echo 'Infrastructure started.' && echo 'api-gateway: http://localhost:8080' && echo 'auth-service: http://localhost:8081' && echo 'user-service: http://localhost:8082' && echo 'RabbitMQ UI: http://localhost:15672' && echo 'pgAdmin: http://localhost:5050' && echo '' && docker compose ps"
+    do script "cd '$ROOT_DIR' && echo 'Starting Docker infrastructure...' && docker compose up -d && echo '' && echo 'Infrastructure started.' && echo 'api-gateway: http://localhost:8080' && echo 'auth-service: http://localhost:8081' && echo 'user-service: http://localhost:8082' && echo 'wallet-service: http://localhost:8083' && echo 'RabbitMQ UI: http://localhost:15672' && echo 'pgAdmin: http://localhost:5050' && echo '' && docker compose ps"
 
     do script "cd '$ROOT_DIR/api-gateway' && echo 'Starting api-gateway on port 8080...' && ./mvnw spring-boot:run"
 
     do script "cd '$ROOT_DIR/auth-service' && echo 'Starting auth-service on port 8081...' && ./mvnw spring-boot:run"
 
     do script "cd '$ROOT_DIR/user-service' && echo 'Starting user-service on port 8082...' && ./mvnw spring-boot:run"
+
+    do script "cd '$ROOT_DIR/wallet-service' && echo 'Starting wallet-service on port 8083...' && ./mvnw spring-boot:run"
 end tell
 EOF
