dd823d30f07379b892e5dcd980ec9f28aa180a67
bojan.vasic <bojan.vasic01@gmail.com>
2026-07-09 11:11:24 +0200

    feat(payment): add payment service skeleton

---
+++ payment-service/src/main/java/com/boki0/casino/payment/PaymentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.boki0.casino.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
